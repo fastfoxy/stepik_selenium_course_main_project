642c4d87b6d7e4050cd93982da717a54640e3f53
odd_bod <odd_bod@mail.ru>
2022-10-20 13:18:54 +0300

    Добавлен файл и класс для страницы Login в структуре Page Object Добавлен файл с локаторами с классами для главной страницы и страницы Login Внесены изменения в класс главной страницы для работы с локаторами во внешнем файле Добавлены 3 теста: наличие login в url страницы Login, наличие формы входа и наличие формы регистрации на странице Login

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,26 @@
 from .pages.main_page import MainPage
-from selenium import webdriver
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+def test_guest_shoul_go_to_login_url(browser):
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_form()
+
+def test_guest_should_see_registration_form(browser):
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_register_form()
