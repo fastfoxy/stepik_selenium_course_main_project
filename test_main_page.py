aea02c5f174132f711205312572062342f49dba8
odd_bod <odd_bod@mail.ru>
2022-10-20 17:14:50 +0300

    Добаввлено неявное открытие страницы Login в тест test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -9,9 +9,11 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link) 
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_shoul_go_to_login_url(browser):
     page = LoginPage(browser, browser.current_url)
